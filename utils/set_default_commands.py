974195e33d1fb29f5683f980187a5aadd128ae24
altcode <cosmosx1328@gmail.com>
2022-08-01 00:56:21 +0600

    :recycle: Remove `/` from commands in set_default_commands()

--- utils/set_default_commands.py
+++ utils/set_default_commands.py
@@ -6,10 +6,10 @@ async def set_default_commands(dp):
         [
             aiogram.types.BotCommand("start", "Start bot"),
             aiogram.types.BotCommand("help", "Display help"),
-            aiogram.types.BotCommand("/add_quote", "Add new quote"),
-            aiogram.types.BotCommand('/cancel', 'Cancel operation'),
-            aiogram.types.BotCommand('/skip', 'Skip one step'),
-            aiogram.types.BotCommand('/quote_menu', 'Show quote menu'),
-            aiogram.types.BotCommand('/tag_menu', 'Show tag menu'),
+            aiogram.types.BotCommand("add_quote", "Add new quote"),
+            aiogram.types.BotCommand('cancel', 'Cancel operation'),
+            aiogram.types.BotCommand('skip', 'Skip one step'),
+            aiogram.types.BotCommand('quote_menu', 'Show quote menu'),
+            aiogram.types.BotCommand('tag_menu', 'Show tag menu'),
         ]
     )
