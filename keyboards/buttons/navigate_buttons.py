c1b16dcc9db919113020e7427c06020cc9cafb2c
altcode <cosmosx1328@gmail.com>
2022-08-02 02:33:33 +0600

    :truck: Create NextButton and PreviousButton

---
+++ keyboards/buttons/navigate_buttons.py
@@ -0,0 +1,12 @@
+import aiogram.types
+from aiogram.utils import callback_data
+
+
+class PreviousButton(aiogram.types.InlineKeyboardButton):
+    def __init__(self, callback_factory: callback_data.CallbackData, page: int):
+        super().__init__('Previous', callback_factory.new(page=page))
+
+
+class NextPreviousButton(aiogram.types.InlineKeyboardButton):
+    def __init__(self, callback_factory: callback_data.CallbackData, page: int):
+        super().__init__('Next', callback_factory.new(page=page))
