9dc76532d885b8b8ad368966a06ed088b4ed0b7c
altcode <cosmosx1328@gmail.com>
2022-08-02 11:36:31 +0600

    :fire: Remove waiting_for_confirmation

--- states/quote_states.py
+++ states/quote_states.py
@@ -5,4 +5,3 @@ class AddQuote(state.StatesGroup):
     waiting_for_quote_content = state.State()
     waiting_for_quote_author = state.State()
     waiting_for_quote_tags = state.State()
-    waiting_for_confirmation = state.State()
