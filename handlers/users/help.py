170b5753a72ed0650c80442d0eda7eb9867e1765
altcode <cosmosx1328@gmail.com>
2022-08-03 02:09:51 +0600

    :recycle: Use HelpResponse

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -2,14 +2,9 @@ from aiogram.dispatcher import filters
 from aiogram import types
 
 from loader import dp
+import responses.help
 
 
 @dp.message_handler(filters.CommandHelp())
 async def get_help(message: types.Message):
-    await message.answer(
-        '/start - Start bot\n'
-        '/help - Get help\n'
-        '/add_quote - Add quote\n'
-        '$$ - Quickly Add quote\n'
-        '(The author is marked with @, tags - #)'
-        )
+    await responses.help.HelpResponse(message)
