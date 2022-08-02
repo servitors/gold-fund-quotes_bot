ca40f53f688ce86f93eead5a8f5300d1bd94b4c7
altcode <cosmosx1328@gmail.com>
2022-08-02 21:18:07 +0600

    :recycle: Add `QuotesKeyboard`

--- keyboards/inline/quote_keyboards.py
+++ keyboards/inline/quote_keyboards.py
@@ -6,6 +6,7 @@ import aiogram
 from keyboards.buttons import quote_buttons, navigate_buttons
 import keyboards.inline.callback_data
 from utils.db_api import schemas
+from keyboards.inline import callback_factories
 
 
 class QuoteMenuKeyboard(aiogram.types.InlineKeyboardMarkup):
@@ -15,11 +16,23 @@ class QuoteMenuKeyboard(aiogram.types.InlineKeyboardMarkup):
         self.add(quote_buttons.EditQuoteTextButton(quote_id))
         self.add(quote_buttons.EditQuoteTextButton(quote_id),
                  quote_buttons.EditQuoteAuthorButton(quote_id))
-        self.add(quote_buttons.QuoteTagsButton(quote_id))Z
+        self.add(quote_buttons.QuoteTagsButton(quote_id))
+
+
+class QuotesKeyboard(aiogram.types.InlineKeyboardMarkup):
+    def __init__(self, quotes: list[schemas.Quote], page: int):
+        super().__init__()
+        self.__quotes = quotes
+        self.__page = page
+        self.add(*[quote_buttons.QuoteButton(quote.content, self.__page) for quote in self.__quotes])
+        callback_factory = callback_factories.QuoteMenuCallbackFactory()
+        self.row(
+            navigate_buttons.PreviousButton(callback_factory=callback_factory, page=self.__page - 1),
+            navigate_buttons.NextPreviousButton(callback_factory=callback_factory, page=self.__page + 1)
+        )
 
 
 class QuoteMenuKeyboard(aiogram.types.InlineKeyboardMarkup):
-
     navigation_buttons_cb = callback_data.CallbackData('quote', 'action', 'page')
 
     def __init__(self, quotes: list[schemas.Quote], page: int, action: str):
