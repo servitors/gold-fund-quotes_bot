e8b0e91f674e91baba48fc34b4879a3df1ee6b02
altcode <cosmosx1328@gmail.com>
2022-08-03 01:29:43 +0600

    :recycle: Use StartResponse in bot_start handler

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,12 +1,13 @@
 from aiogram.dispatcher import filters
 from aiogram import types
 
+from responses import start
 from utils import db_api
 from loader import dp
 
 
 @dp.message_handler(filters.builtin.CommandStart())
 async def bot_start(message: types.Message):
-    name = message.from_user.full_name
-    await message.answer(f"Hello, {name}")
-    db_api.add_user_in_db(message.from_user.id, name)
+    full_name = message.from_user.full_name
+    await start.StartResponse(message, full_name)
+    db_api.add_user_in_db(message.from_user.id, full_name)
