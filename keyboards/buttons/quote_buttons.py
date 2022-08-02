7d4bca274f45fe3019abf87d1a0fc6c8378bdbd8
altcode <cosmosx1328@gmail.com>
2022-08-02 19:36:34 +0600

    :recycle: Create `EditQuoteTextButton`

--- keyboards/buttons/quote_buttons.py
+++ keyboards/buttons/quote_buttons.py
@@ -3,6 +3,14 @@ import aiogram.types
 from keyboards.inline import callback_factories
 
 
+class EditQuoteTextButton(aiogram.types.InlineKeyboardButton):
+    def __init__(self, quote_id: int):
+        callback_factory = callback_factories.QuoteMenuCallbackFactory()
+        super().__init__('Изменить текст', callback_data=callback_factory.new(
+            action='edit_text', id=quote_id
+        ))
+
+
 class EditQuoteAuthorButton(aiogram.types.InlineKeyboardButton):
     def __init__(self, quote_id: int):
         callback_factory = callback_factories.QuoteMenuCallbackFactory()
