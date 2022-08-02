f6c96dedec0c7eb2f2832bc6b36bc3dbf9d400e4
altcode <cosmosx1328@gmail.com>
2022-08-02 22:14:49 +0600

    :recycle: Change page to tag_id

--- keyboards/buttons/tag_buttons.py
+++ keyboards/buttons/tag_buttons.py
@@ -4,6 +4,6 @@ from keyboards.inline import callback_factories
 
 
 class TagButton(aiogram.types.InlineKeyboardButton):
-    def __init__(self, text: str, page: int):
+    def __init__(self, text: str, tag_id: int):
         callback_factory = callback_factories.TagsCallbackFactory()
-        super().__init__(text, callback_data=callback_factory.new(page=page))
+        super().__init__(text, callback_data=callback_factory.new(tag_id))
