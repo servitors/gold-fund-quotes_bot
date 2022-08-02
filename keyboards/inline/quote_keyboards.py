b1ecd164511d28ddd73e9b3d2ea747fd9eb79b31
altcode <cosmosx1328@gmail.com>
2022-08-02 19:45:11 +0600

    :fire: Remove `EditQuoteKeyboard`

--- keyboards/inline/quote_keyboards.py
+++ keyboards/inline/quote_keyboards.py
@@ -15,39 +15,7 @@ class QuoteMenuKeyboard(aiogram.types.InlineKeyboardMarkup):
         self.add(quote_buttons.EditQuoteTextButton(quote_id))
         self.add(quote_buttons.EditQuoteTextButton(quote_id),
                  quote_buttons.EditQuoteAuthorButton(quote_id))
-        self.add(quote_buttons.QuoteTagsButton(quote_id))
-
-
-class EditQuoteKeyboard(InlineKeyboardMarkup):
-
-    def __init__(self, quote_id, page):
-        super(EditQuoteKeyboard, self).__init__(row_width=2)
-        self.quote_id = quote_id
-        self.page = page
-        self.add(*self.quote_buttons)
-        self.add(self.back_button)
-
-    @property
-    def quote_buttons(self):
-        return (
-            InlineKeyboardButton(text='Изменить цитату', callback_data=quote_cb.new(
-                action='edit_content',
-                id=self.quote_id)),
-            InlineKeyboardButton(text='Изменить автора', callback_data=quote_cb.new(
-                action='edit_author',
-                id=self.quote_id)),
-            InlineKeyboardButton(text='Теги', callback_data=quote_cb.new(
-                action='tags',
-                id=self.quote_id))
-        )
-
-    @property
-    def back_button(self):
-        return InlineKeyboardButton(
-            text='Назад',
-            callback_data=QuoteMenuKeyboard.navigation_buttons_cb.new(
-                action='navigate', page=self.page)
-        )
+        self.add(quote_buttons.QuoteTagsButton(quote_id))Z
 
 
 class QuoteMenuKeyboard(aiogram.types.InlineKeyboardMarkup):
