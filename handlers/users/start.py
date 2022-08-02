9cca5bc8e80d0edeca8ab0f52011934cb93ca243
altcode <cosmosx1328@gmail.com>
2022-08-03 01:30:11 +0600

    :recycle: Rename bot_start to start

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,7 +7,7 @@ from loader import dp
 
 
 @dp.message_handler(filters.builtin.CommandStart())
-async def bot_start(message: types.Message):
+async def start(message: types.Message):
     full_name = message.from_user.full_name
     await start.StartResponse(message, full_name)
     db_api.add_user_in_db(message.from_user.id, full_name)
