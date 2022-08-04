b3062e7918f3be4c4672a2a4f19c6dd04b26e883
altcode <cosmosx1328@gmail.com>
2022-08-05 01:18:28 +0600

    🗃️ Edit queries with SQLAlchemy query style 2.0

--- src/utils/db_api/queries.py
+++ src/utils/db_api/queries.py
@@ -1,5 +1,8 @@
+import contextlib
 import typing
 
+import sqlalchemy
+import sqlalchemy.exc
 from sqlalchemy.dialects import postgresql
 from sqlalchemy import orm
 
@@ -8,76 +11,81 @@ from utils.db_api import base
 
 
 def add_user_in_db(session: orm.Session, telegram_id: int, name: str) -> None:
-    statement = postgresql.insert(schemas.User).values(id=telegram_id, name=name)
-    statement = statement.on_conflict_do_nothing(index_elements=[schemas.User.id])
-    session.execute(statement)
+    session.add(schemas.User(telegram_id=telegram_id, name=name))
 
 
 def add_quote_in_db(session: orm.Session, user_id: int, **kwargs) -> None:
-    session.add(schemas.Quote(user_id=user_id, order_in_user=count_quote(user_id), **kwargs))
+    session.add(schemas.Quote(
+        user_id=user_id, order_in_user=count_user_quotes(session, user_id), **kwargs)
+    )
 
 
 def add_tag_in_db(session: orm.Session, name: str, user_id: int) -> None:
-    session.add(schemas.Tag(name=name, user_id=user_id, order_in_user=count_tags(user_id)))
+    session.add(schemas.Tag(
+        name=name, user_id=user_id, order_in_user=count_user_tags(session, user_id))
+    )
 
 
 def bind_tag_to_quote(session: orm.Session, tag_id: int, quote_id: int):
     session.add(schemas.QuoteTag(quote_id=quote_id, tag_id=tag_id))
 
 
-def get_user_by_id(session: orm.Session, user_id: int) -> schemas.User:
-    return session.query(schemas.User).filter_by(id=user_id).one()
+def get_user_by_id(session: orm.Session, user_id: int) -> schemas.User | None:
+    return session.get(schemas.User, user_id)
 
 
 def get_quotes_by_tags(session: orm.Session, user_id: int, tags: list[str]) -> list[schemas.Quote | None]:
-    quotes = get_user_quotes(session, user_id)
-    if tags:
-        return [quote for quote in quotes if sorted([tag.name for tag in quote.tag]) == sorted(tags)]
-    else:
-        return quotes
+    ...
 
 
-def get_user_quotes(session: orm.Session, user_id: int) -> list[schemas.Quote | None]:
-    return [quote for quote in session.query(schemas.Quote).filter(schemas.Quote.user_id == user_id)]
+def get_user_quotes(
+        session: orm.Session, user_id: int,
+        page: int = None, page_size: int = None) -> list[schemas.Quote | None]:
+
+    statement = sqlalchemy.select(schemas.Quote).filter_by(user_id == user_id)
+    statement = statement.order_by('created_at')
+    if page and page_size:
+        statement = statement.limit(page_size).offset(page * page_size)
+    return session.scalars(statement).all()
 
 
-def get_user_quotes_in_range(session: orm.Session, user_id: int, quote_range: range) -> list[schemas.Quote | None]:
-    return [
-        quote for quote in session.query(schemas.Quote).filter(
-            schemas.Quote.user_id == user_id,
-            schemas.Quote.order_in_user.in_(quote_range)
-        )
-    ]
-
-
-def get_user_tags(session: orm.Session, user_id: int) -> list[schemas.Quote | None]:
-    return session.query(schemas.Tag).filter_by(user_id=user_id)
-
-
-def get_user_tags_in_range(session: orm.Session, user_id: int, tag_range: range):
-    return [
-        tag for tag in session.query(schemas.Tag).filter(
-            schemas.Tag.user_id == user_id,
-            schemas.Tag.order_in_user.in_(tag_range)
-        )
-    ]
+def get_user_tags(session: orm.Session, user_id: int,
+                  page: int = None, page_size: int = None) -> list[schemas.Tag | None]:
+    statement = sqlalchemy.select(schemas.Tag).filter_by(user_id == user_id).order_by('created_at')
+    if page and page_size:
+        statement = statement.limit(page_size).offset(page * page_size)
+    return session.scalars(statement).all()
 
 
 def update_quote(session: orm.Session, quote_id: int, **kwargs) -> None:
-    session.query(schemas.Quote).filter_by(id=quote_id).update(kwargs)
+    session.execute(
+        sqlalchemy.update(schemas.Quote).
+        where(schemas.Quote.id == quote_id).
+        values(**kwargs)
+    )
 
 
 def delete_quote(session: orm.Session, quote_id: int) -> None:
-    session.query(schemas.Quote).filter_by(id=quote_id).delete()
+    session.execute(
+        sqlalchemy.delete(schemas.Tag).where(schemas.Quote.id == quote_id)
+    )
 
 
 def delete_tag(session: orm.Session, tag_id: int) -> None:
-    session.query(schemas.Tag).filter_by(id=tag_id).delete()
+    session.execute(
+        sqlalchemy.delete(schemas.Tag).where(schemas.Tag.id == tag_id)
+    )
 
 
 def count_user_quotes(session: orm.Session, user_id: int) -> int:
-    return session.query(schemas.Quote).filter_by(user_id=user_id).count()
+    statement = sqlalchemy.select(
+        sqlalchemy.func.count(schemas.Quote.id)
+    ).filter_by(user_id=user_id)
+    return session.scalar(statement)
 
 
 def count_user_tags(session: orm.Session, user_id: int) -> int:
-    return session.query(schemas.Tag).filter_by(user_id=user_id).count()
+    statement = sqlalchemy.select(
+        sqlalchemy.func.count(schemas.Tag.id)
+    ).filter_by(user_id=user_id)
+    return session.scalar(statement)
