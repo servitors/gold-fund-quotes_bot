9dc320919d36232dba655cbdb47e3c2240592195
altcode <cosmosx1328@gmail.com>
2022-08-05 15:40:47 +0600

    :bug: Resolve name conflict, use `response.start` instead of `start`

--- src/handlers/users/start.py
+++ src/handlers/users/start.py
@@ -1,13 +1,13 @@
 from aiogram.dispatcher import filters
 import aiogram.types
 
-from responses import start
 from utils import db_api
+import responses.start
 from loader import dp
 
 
 @dp.message_handler(filters.builtin.CommandStart())
 async def start(message: aiogram.types.Message):
     full_name = message.from_user.full_name
-    await start.StartResponse(message, full_name)
+    await responses.start.StartResponse(message, full_name)
     db_api.add_user_in_db(message.from_user.id, full_name)
