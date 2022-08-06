fabedbdecd92dc23399ba1cc7408d61dd6f9d439
altcode <cosmosx1328@gmail.com>
2022-08-06 23:11:40 +0600

    :recycle: Edit quote_id to quote

--- src/responses/quote.py
+++ src/responses/quote.py
@@ -1,14 +1,15 @@
 import aiogram.types
 
 from keyboards.inline import quote_keyboards
+from utils.db_api import schemas
 from responses import base
 
 
 class QuoteMenuResponse(base.BaseResponse):
-    def __init__(self, callback: aiogram.types.CallbackQuery, quote_id: int):
+    def __init__(self, callback: aiogram.types.CallbackQuery, quote: schemas.Quote):
         self.__callback = callback
-        self.__quote_id = quote_id
+        self.__quote = quote
 
     async def _send_response(self):
-        keyboard = quote_keyboards.QuoteMenuKeyboard(quote_id=self.__quote_id)
+        keyboard = quote_keyboards.QuoteMenuKeyboard(quote_id=self.__quote)
         await self.__callback.message.answer('Quote Menu', reply_markup=keyboard)
