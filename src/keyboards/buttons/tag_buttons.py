ad9cb81e4c1b71b6f411b4aebcc1273522be59aa
altcode <cosmosx1328@gmail.com>
2022-08-07 23:03:27 +0600

    :recycle: Change TagsCallbackFactory to TagMenuCallbackFactory

--- src/keyboards/buttons/tag_buttons.py
+++ src/keyboards/buttons/tag_buttons.py
@@ -5,5 +5,5 @@ from keyboards.inline import callback_factories
 
 class TagButton(aiogram.types.InlineKeyboardButton):
     def __init__(self, text: str, tag_id: int):
-        callback_factory = callback_factories.TagsCallbackFactory()
+        callback_factory = callback_factories.TagMenuCallbackFactory()
         super().__init__(text, callback_data=callback_factory.new(tag_id))
