db3eb5e7405a13c9d7c193ca8920d3efeeb7346b
altcode <cosmosx1328@gmail.com>
2022-08-07 20:16:58 +0600

    :sparkles: Create QuotesResponse

--- src/handlers/users/quote/get.py
+++ src/handlers/users/quote/get.py
@@ -1,6 +1,7 @@
 from aiogram.dispatcher import filters
 import aiogram.types
 
+import responses.quote
 from utils import db_api
 import utils.db_api.session
 from loader import dp
@@ -30,20 +31,14 @@ async def get_quote_in_inline_mode(query: aiogram.types.InlineQuery):
 
 @dp.message_handler(filters.Command('quote_menu'))
 async def quote_menu(message: aiogram.types.Message):
-    user_id = message.from_user.id
     with db_api.session.Session() as session, session.begin():
-        quotes = db_api.get_user_quotes(session, user_id, page=0, page_size=10)
-    menu = quote_menu.QuoteMenuKeyboard(quotes, page=0, action='select')
-    await message.answer(text='Quote Menu', reply_markup=menu)
+        quotes = db_api.get_user_quotes(session, message.from_user.id, page=0, page_size=10)
+    await responses.quote.QuotesResponse(message, quotes)
 
 
 @dp.callback_query_handler(quote_menu.QuoteMenuKeyboard.navigation_buttons_cb.filter(action='navigate'))
 async def navigate_quote_menu(query: aiogram.types.CallbackQuery, callback_data: dict):
-    user_id = query.from_user.id
     page = callback_data['page']
     with db_api.session.Session() as session, session.begin():
-        quotes = db_api.get_user_quotes(session, user_id, page=page, page_size=10)
-    menu = quote_menu.QuoteMenuKeyboard(quotes, page=page, action='select')
-    await query.message.edit_text(text='Quote Menu')
-    await query.message.edit_reply_markup(reply_markup=menu)
-    await query.answer()
+        quotes = db_api.get_user_quotes(session, query.from_user.id, page=page, page_size=10)
+    await responses.quote.QuotesResponse(query, quotes, page)
--- src/responses/quote.py
+++ src/responses/quote.py
@@ -17,3 +17,22 @@ class QuoteMenuResponse(base.BaseResponse):
             utils.quote.quote_constructor(
                 self.__quote.content, self.__quote.author))
         await self.__callback.message.edit_reply_markup(keyboard)
+
+
+class QuotesResponse(base.BaseResponse):
+    def __init__(self, update: aiogram.types.CallbackQuery | aiogram.types.Message,
+                 quotes: list[schemas.Quote], page: int = 0):
+        self.__update = update
+        self.__page = page
+        self.__quotes = quotes
+
+    async def _send_response(self):
+        if isinstance(self.__update, aiogram.types.CallbackQuery):
+            await self.__update.message.edit_text('Quotes')
+            await self.__update.message.edit_reply_markup(
+                quote_keyboards.QuotesKeyboard(self.__quotes, page=self.__page)
+            )
+        elif isinstance(self.__update, aiogram.types.Message):
+            await self.__update.answer(
+                'Quotes', reply_markup=quote_keyboards.QuotesKeyboard(self.__quotes, self.__page)
+            )
