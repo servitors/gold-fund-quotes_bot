97c50796600a66542eb9f725647ac5d7698611d8
altcode <cosmosx1328@gmail.com>
2022-08-07 19:11:49 +0600

    :recycle: Change get_user_quotes_in_range to get_user_quotes

--- src/handlers/users/quote/get.py
+++ src/handlers/users/quote/get.py
@@ -33,7 +33,7 @@ async def get_quote_in_inline_mode(query: aiogram.types.InlineQuery):
 async def quote_menu(message: aiogram.types.Message):
     user_id = message.from_user.id
     with db_api.session.Session() as session, session.begin():
-        quotes = db_api.get_user_quotes_in_range(session, user_id, range(0, 10))
+        quotes = db_api.get_user_quotes(session, user_id, page=0, page_size=10)
     menu = quote_menu.QuoteMenuKeyboard(quotes, page=0, action='select')
     await message.answer(text='Quote Menu', reply_markup=menu)
 
@@ -41,12 +41,12 @@ async def quote_menu(message: aiogram.types.Message):
 @dp.callback_query_handler(quote_menu.QuoteMenuKeyboard.navigation_buttons_cb.filter(action='navigate'))
 async def navigate_quote_menu(query: aiogram.types.CallbackQuery, callback_data: dict):
     user_id = query.from_user.id
-    elements_on_page = 10
+    page = callback_data['page']
     with db_api.session.Session() as session, session.begin():
         quantity = db_api.count_quote(session, user_id)
     paginator = pagination.Pagination(quantity, int(callback_data['page']), elements_on_page)
     with db_api.session.Session() as session, session.begin():
-        quotes = db_api.get_user_quotes_in_range(session, user_id, paginator.range_elements)
+        quotes = db_api.get_user_quotes(session, user_id, page=page, page_size=10)
     menu = quote_menu.QuoteMenuKeyboard(quotes, paginator.page, action='select')
     await query.message.edit_text(text='Quote Menu')
     await query.message.edit_reply_markup(reply_markup=menu)
