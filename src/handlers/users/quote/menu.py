3d0ca0852a23e7798769c64b0a5398b0f44b7af9
altcode <cosmosx1328@gmail.com>
2022-08-07 18:40:08 +0600

    :recycle: Rename get_quote_by_order_in_user to get_user_by_id

--- src/handlers/users/quote/menu.py
+++ src/handlers/users/quote/menu.py
@@ -10,5 +10,5 @@ import responses.quote
 @dp.callback_query_handler(keyboards.inline.callback_data.quote_cb.filter(action='select'))
 async def get_quote(query: aiogram.types.CallbackQuery, callback_data: dict):
     with db_api.session.Session() as session, session.begin():
-        quote = db_api.get_quote_by_order_in_user(session, int(callback_data['id']))
+        quote = db_api.get_user_by_id(session, int(callback_data['id']))
     await responses.quote.QuoteMenuResponse(query, quote)
