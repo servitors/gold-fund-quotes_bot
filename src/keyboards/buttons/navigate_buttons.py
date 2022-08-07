51eff79f4e988a443e125c19dc492f0eb9f24569
altcode <cosmosx1328@gmail.com>
2022-08-07 22:51:46 +0600

    :recycle: Add callback_data to buttons

--- src/keyboards/buttons/navigate_buttons.py
+++ src/keyboards/buttons/navigate_buttons.py
@@ -4,9 +4,9 @@ from aiogram.utils import callback_data
 
 class PreviousButton(aiogram.types.InlineKeyboardButton):
     def __init__(self, callback_factory: callback_data.CallbackData, page: int):
-        super().__init__('Previous', callback_factory.new(page=page))
+        super().__init__('Previous', callback_data=callback_factory.new(page=page))
 
 
 class NextPreviousButton(aiogram.types.InlineKeyboardButton):
     def __init__(self, callback_factory: callback_data.CallbackData, page: int):
-        super().__init__('Next', callback_factory.new(page=page))
+        super().__init__('Next', callback_data=callback_factory.new(page=page))
