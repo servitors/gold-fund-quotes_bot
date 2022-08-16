44526ed11496178e9f1c6fc8e3027fa8e1992515
altcode <cosmosx1328@gmail.com>
2022-08-16 18:09:24 +0600

    :sparkles: Create create_session() contextmanager

--- src/services/db_api/session.py
+++ src/services/db_api/session.py
@@ -1,5 +1,15 @@
+import contextlib
+import typing
+
 from sqlalchemy import orm
 
 from services.db_api import engine
 
+
 RawSession = orm.sessionmaker(bind=engine.engine)
+
+
+@contextlib.contextmanager
+def create_session() -> typing.Generator[orm.Session, None, None]:
+    with RawSession() as session, session.begin():
+        yield session
